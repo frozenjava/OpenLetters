2a49c76bbf404d77bd7b276ebc9b6b45469cbc85
Josh <josh@frozendevelopment.net>
2025-01-25 14:00:30 -0500

    more stuff to prepare for release

--- app/src/main/java/net/frozendevelopment/openletters/migration/AppMigrationKoin.kt
+++ app/src/main/java/net/frozendevelopment/openletters/migration/AppMigrationKoin.kt
@@ -2,7 +2,6 @@ package net.frozendevelopment.openletters.migration
 
 import net.frozendevelopment.openletters.data.sqldelight.AppMigrationQueries
 import net.frozendevelopment.openletters.data.sqldelight.CategoryQueries
-import net.frozendevelopment.openletters.data.sqldelight.LetterQueries
 import org.koin.core.annotation.Factory
 import org.koin.core.annotation.Module
 
@@ -10,16 +9,14 @@ import org.koin.core.annotation.Module
 class AppMigrationKoin {
     @Factory
     fun appMigrator(
-        appMigrationQueries: net.frozendevelopment.openletters.data.sqldelight.AppMigrationQueries,
-        categoryQueries: net.frozendevelopment.openletters.data.sqldelight.CategoryQueries,
-        letterQueries: LetterQueries,
+        appMigrationQueries: AppMigrationQueries,
+        categoryQueries: CategoryQueries,
     ): AppMigrator {
         return AppMigrator(
             appMigrationQueries = appMigrationQueries,
             migrations =
                 listOf(
                     InitialCategoriesMigration(categoryQueries),
-                    TestDataMigration(letterQueries),
                 ),
         )
     }
