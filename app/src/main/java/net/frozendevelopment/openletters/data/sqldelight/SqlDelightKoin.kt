b32c9a1ff77476a95f8ae0c763aef56bea5cc53d
Josh <josh@frozendevelopment.net>
2025-12-22 14:53:40 -0500

    fixed tests

--- app/src/main/java/net/frozendevelopment/openletters/data/sqldelight/SqlDelightKoin.kt
+++ app/src/main/java/net/frozendevelopment/openletters/data/sqldelight/SqlDelightKoin.kt
@@ -3,18 +3,7 @@ package net.frozendevelopment.openletters.data.sqldelight
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import io.requery.android.database.sqlite.RequerySQLiteOpenHelperFactory
-import net.frozendevelopment.openletters.data.sqldelight.migrations.Category
-import net.frozendevelopment.openletters.data.sqldelight.migrations.Document
-import net.frozendevelopment.openletters.`data`.sqldelight.migrations.Letter
-import net.frozendevelopment.openletters.data.sqldelight.migrations.LetterToCategory
-import net.frozendevelopment.openletters.data.sqldelight.migrations.LetterToReminder
-import net.frozendevelopment.openletters.data.sqldelight.migrations.Reminder
-import net.frozendevelopment.openletters.data.sqldelight.models.CategoryId
-import net.frozendevelopment.openletters.data.sqldelight.models.ColorAdapter
-import net.frozendevelopment.openletters.data.sqldelight.models.DocumentId
-import net.frozendevelopment.openletters.data.sqldelight.models.LetterId
-import net.frozendevelopment.openletters.data.sqldelight.models.LocalDateTimeAdapter
-import net.frozendevelopment.openletters.data.sqldelight.models.ReminderId
+import net.frozendevelopment.openletters.extensions.invoke
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 // import org.koin.core.annotation.Factory
@@ -108,45 +97,7 @@ val sqlDelightKoinModule =
 
         single {
             // Enable foreign keys on the driver instance used by the DB
-            val driver = get<SqlDriver>().apply { execute(null, "PRAGMA foreign_keys = ON;", 0) }
-            OpenLettersDB(
-                driver = driver,
-                letterAdapter =
-                    Letter.Adapter(
-                        idAdapter = LetterId.adapter,
-                        createdAdapter = LocalDateTimeAdapter,
-                        lastModifiedAdapter = LocalDateTimeAdapter,
-                    ),
-                documentAdapter =
-                    Document.Adapter(
-                        idAdapter = DocumentId.adapter,
-                        letterIdAdapter = LetterId.adapter,
-                    ),
-                categoryAdapter =
-                    Category.Adapter(
-                        idAdapter = CategoryId.adapter,
-                        colorAdapter = ColorAdapter,
-                        createdAdapter = LocalDateTimeAdapter,
-                        lastModifiedAdapter = LocalDateTimeAdapter,
-                    ),
-                letterToCategoryAdapter =
-                    LetterToCategory.Adapter(
-                        letterIdAdapter = LetterId.adapter,
-                        categoryIdAdapter = CategoryId.adapter,
-                    ),
-                reminderAdapter =
-                    Reminder.Adapter(
-                        idAdapter = ReminderId.adapter,
-                        createdAdapter = LocalDateTimeAdapter,
-                        lastModifiedAdapter = LocalDateTimeAdapter,
-                        scheduledForAdapter = LocalDateTimeAdapter,
-                    ),
-                letterToReminderAdapter =
-                    LetterToReminder.Adapter(
-                        letterIdAdapter = LetterId.adapter,
-                        reminderIdAdapter = ReminderId.adapter,
-                    ),
-            )
+            OpenLettersDB(get())
         }
 
         factory { get<OpenLettersDB>().reminderQueries }
